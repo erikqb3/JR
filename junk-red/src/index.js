f2b204605075467375c729a59080b0ac29b55601
erikqb3 <erikqb3@gmail.com>
2023-05-29 21:30:41 -0600

    Step 4: change in index.js

--- junk-red/src/index.js
+++ junk-red/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+function App(){
+  return (
+    <h1>WORKING APP</h1>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
